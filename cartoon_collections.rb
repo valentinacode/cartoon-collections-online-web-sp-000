e34caa685858dcfb649f53e7375b6bd4afac42a8
Valentina E <tiyahelfrih@gmail.com>
2020-08-06 17:07:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |  
+  planeteer_calls.any? { | call  | call.length > 4 }
 
 def find_the_cheese# code an argument here
   # the array below is here to help
